80adfec4c0f6d8ded174f4d21f3b8e713c307790
hianisa <dehghanzadeh655@yahoo.com>
2026-09-08 14:01:21 +0200

    mondriaan

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,53 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
-function draw() {
-  background(220);
+function draw(){
+ 
+  fill ("#ffde23")
+  rect (0, 0, 265, 95)
+
+ 
+fill ("#000000")
+  rect (0, 95, 50, 200)
+
+fill ("#c40000")
+ rect (0, 200, 500, 250)
+
+ fill ("#ffde23")
+ rect (0, 550, 300, 170)
+
+fill ("#002c92")
+ rect (0, 450, 100, 100)
+
+fill ("#002c92")
+ rect (265, 0, 100, 200)
+
+fill ("#ffde23")
+ rect (500, 0, 100, 400)
+
+fill ("#002c92")
+ rect (500, 400, 300, 320)
+
+fill ("#002c92")
+ rect (600, 0, 200, 60)
+
+fill ("#000000")
+ rect (300, 450, 200, 270)
+
+fill ("#000000")
+ rect (600, 60, 50 ,200)
+
+fill ("#c40000")
+ rect (600, 250, 200, 150)
+
+
+
+
+
+
+
+
 }
+
+ 
